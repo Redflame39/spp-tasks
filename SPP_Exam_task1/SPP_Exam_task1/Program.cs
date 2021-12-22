8a05487f443ddefadb334fc7e6c13dd7600821aa
Rafael Pavlov <pavel02rafeev@gmail.com>
2021-12-22 17:20:54 +0300

    task completed with comments provided

---
+++ SPP_Exam_task1/SPP_Exam_task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SPP_Exam_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue queue = new TaskQueue(10);
+            for(int i=0; i< 10; i++)
+            {
+                queue.EnqueueTask(ExampleTask);
+            }
+        }
+
+        static void ExampleTask()
+        {
+            for(int i=0; i< 15; i++)
+            {
+                Console.Error.WriteLine($"Number: {i}");
+            }
+        }
+    }
+}
