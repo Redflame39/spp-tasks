91a9e015ba03066ac7cbaa321940f2dc08ee6ad6
Redflame39 <vanya133719@gmail.com>
2021-12-22 14:26:28 +0300

    NativeBuffer implemented

---
+++ NativeBuffer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class NativeBuffer : IDisposable
+{
+    // Дескриптор выделенного блока. Только отдаем, устанавливать запрещаем
+    public IntPtr Handle { get; }
+
+    // sizeBytes - размер выделяемого блока
+    public NativeBuffer(int sizeBytes)
+    {
+        try
+        {
+            Handle = Marshal.AllocHGlobal(sizeBytes); // выделение
+        }
+        catch (OutOfMemoryException) // нету свободной памяти
+        { 
+            Console.WriteLine("There is insufficient memory to satisfy the request.");
+        }
+    }
+
+    // деструктор на этапе компиляции превратится в Finalize
+    ~NativeBuffer()
+    {
+        Marshal.FreeHGlobal(Handle); // освобождаем
+    }
+
+    // для ручной очистки
+    public void Dispose()
+    {
+        Marshal.FreeHGlobal(Handle); // освобождаем
+    }
+}
